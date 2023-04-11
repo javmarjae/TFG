08ce0c152d36eeb22d09670729e7701dcd9e3f02
Javier Martínez Jaén <javmarjae@gmail.com>
2023-04-11 13:57:23 +0200

    Solución de errores de sonar

--- Proyecto/Gamers/authentication/views.py
+++ Proyecto/Gamers/authentication/views.py
@@ -9,7 +9,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from django.core.mail import EmailMessage
 from django.db.models import Q
-
+from django.core.exceptions import ObjectDoesNotExist
 import web.forms as forms
 import web.models as models
 from .decorators import user_not_authenticated
@@ -21,7 +21,7 @@ def activate(request, uidb64, token):
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
         user = models.User.objects.get(pk=uid)
-    except:
+    except(TypeError,ValueError,OverflowError,ObjectDoesNotExist):
         user = None
 
     if user is not None and account_activation_token.check_token(user, token):
@@ -34,7 +34,7 @@ def activate(request, uidb64, token):
 
     return redirect('index')
 
-def activateEmail(request, user, to_email):
+def activate_email(request, user, to_email):
     mail_subject = "Activate your user account."
     message = render_to_string("template_activate_account.html", {
         'user': user.username,
@@ -58,7 +58,7 @@ def register(request):
             user = form.save(commit=False)
             user.is_active=False
             user.save()
-            activateEmail(request, user, form.cleaned_data.get('email'))
+            activate_email(request, user, form.cleaned_data.get('email'))
             return redirect('index')
         else:
             for error in list(form.errors.values()):
@@ -173,12 +173,12 @@ def password_reset_request(request):
         context={"form": form}
         )
 
-def passwordResetConfirm(request, uidb64, token):
-    User = get_user_model()
+def password_reset_confirm(request, uidb64, token):
+    user = get_user_model()
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except:
+        user = user.objects.get(pk=uid)
+    except(TypeError,ValueError,OverflowError,ObjectDoesNotExist):
         user = None
 
     if user is not None and account_activation_token.check_token(user, token):
